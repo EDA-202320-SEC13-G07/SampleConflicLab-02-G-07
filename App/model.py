31380d47ea05df0d0b89b08406a1706fafef0b98
AndresFsantana <af.santana@uniandes.edu.co>
2023-08-21 20:16:35 -0500

    Revert "“cambios del Estudiante 1”"
    This reverts commit 91af4b3ed607b8cad66a1c5ae888ae0ef907ddb4.

--- App/model.py
+++ App/model.py
@@ -75,9 +75,7 @@ def addBookTags(catalog, booktagsfile):
     Esta funcion guardar los booktags provenientes del archivo CSV.
     """
     # TODO: Mods de Est-1, Est-2 y Est-3 en el Lab 2
-    catalog["book_tags"] = lt.newList(datastructure="ARRAY_LIST",
-                                      filename=booktagsfile)
-    return catalog
+    pass
 
 
 def addTag(catalog, tag):
@@ -104,8 +102,7 @@ def createBookTagList(catalog):
     Esta funcion crea una lista vacia para booktags.
     """
     # TODO: Mods de Est-1, Est-2 y Est-3 en el Lab 2
-    catalog["book_tags"] = lt.newList(datastructure="ARRAY_LIST")
-    return catalog
+    pass
 
 
 def addBookTag(catalog, booktag):
@@ -113,8 +110,7 @@ def addBookTag(catalog, booktag):
     Esta funcion agrega un elemento a lista de booktags.
     """
     # TODO: Mods de Est-1, Est-2 y Est-3 en el Lab 2
-    lt.addLast(catalog["book_tags"], booktag)
-    return catalog
+    pass
 
 
 # Funciones de consulta
